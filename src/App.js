ce242fec6251d55e7e6ce1e76ec474dd568fcfd8
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-03-27 13:28:29 -0300

    Função de deletar mensagem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 import SandMassage from "./components/SandMassage";
 
@@ -6,16 +7,16 @@ function App() {
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
   const [arr, setArr] = useState([
-    { name: "SandMassage", message: "hdbehbd" },
-    { name: "SandMassage", message: "hdbehbd" },
-    { name: "SandMassage", message: "hdbehbd" },
-    { name: "SandMassage", message: "hdbehbd" },
-    { name: "SandMassage", message: "hdbehbd" },
+    { id: "1yh89-oundfcf",name: "SandMassage", message: "hdbehbd" },
   ]);
+  const [click, setClick] = useState(0);
+  const [select, setSelect] = useState({});
 
+  //Verificando ser a conteudo dentro nas variaveis
   function handleSandMassage() {
     if (!!name && !!message) {
       let objMessage = {
+        id: uuidv4(),
         name,
         message,
       };
@@ -25,6 +26,35 @@ function App() {
     }
   }
 
+  //Conta os clicks
+  function handleClick(item) {
+    setSelect(item);
+    
+    //Difencia os clicks 
+    if(item !== select) {
+      setClick(1);
+
+    }else{
+      setClick(click + 1);
+
+    }
+  }
+
+  function deleteMessage(item) {
+      const filterArrays = arr.filter(itemSearche => itemSearche.id !== item.id)
+      setArr(filterArrays)
+  }
+
+  //Verifica os clicks em tempo real
+  useEffect(() => {
+    if (click == 2) {
+      deleteMessage(select)
+      setClick(0);
+    }
+    
+    // alert(click);
+  }, [click]);
+
   return (
     <div className="Container">
       <div className="Container-mensagens">
@@ -32,7 +62,10 @@ function App() {
           arr.map((item) => {
             if (item.name === "eu") {
               return (
-                <div className="Container-componente-Eu">
+                <div
+                  className="Container-componente-Eu"
+                  onClick={() => handleClick(item)}
+                >
                   <div className="componenteEu">
                     <h5>{item.name}</h5>
                     <p>{item.message}</p>
@@ -41,7 +74,8 @@ function App() {
               );
             } else {
               return (
-                <div className="Container-componente-Proximo">
+                <div className="Container-componente-Proximo"
+                onClick={() => handleClick(item)}>
                   <div className="componenteProximo">
                     <h6>{item.name}</h6>
                     <p>{item.message}</p>
